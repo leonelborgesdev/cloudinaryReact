3bb3a1fc44ad46774e1b2cd9381240e4420e0f24
leonelborgesdev <leonel03t@gmail.com>
2023-07-25 09:22:00 -0400

    react cloudinary

--- src/components/MultiplesImagenes.jsx
+++ src/components/MultiplesImagenes.jsx
@@ -1,10 +1,11 @@
+import axios from "axios";
 import { useState } from "react";
 import Dropzone from "react-dropzone";
 import { Container } from "reactstrap";
 
 export const MultiplesImagenes = (props) => {
   const [image, setImage] = useState({ array: {} });
-  const [Loading, setLoading] = useState("");
+  const [loading, setLoading] = useState("");
   const handleDrop = (files) => {
     const uploaders = files.map((file) => {
       const formData = new FormData();
@@ -13,6 +14,24 @@ export const MultiplesImagenes = (props) => {
       formData.append("upload_preset", "Learning");
       formData.append("api_key", "155765619734214");
       formData.append("timestamp", (Date.now() / 1000) | 0);
+      setLoading("true");
+      return axios
+        .post(
+          "https://api.cloudinary.com/v1_1/dt9rc5b91/image/upload",
+          formData,
+          {
+            headers: { "X-Requested-With": "XMLHttpRequest" },
+          }
+        )
+        .then((response) => {
+          const data = response.data;
+          console.log(data);
+          const fileURL = data.secure_url;
+          console.log(fileURL);
+        });
+    });
+    axios.all(uploaders).then(() => {
+      setLoading("false");
     });
   };
   return (
@@ -21,7 +40,9 @@ export const MultiplesImagenes = (props) => {
         <h1 className="text-center">Sube tus imagenes aqui</h1>
         <Dropzone
           clasname="dropzone"
+          onDrop={handleDrop}
           onChange={(e) => setImage(e.target.value)}
+          value={image}
         >
           {({ getRootProps, getInputProps }) => (
             <section>
