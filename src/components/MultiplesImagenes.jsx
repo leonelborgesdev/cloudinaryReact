f405345768aad8ca941c180a08fc7f2467de69f4
leonelborgesdev <leonel03t@gmail.com>
2023-07-25 09:41:47 -0400

    prueba archiv o

--- src/components/MultiplesImagenes.jsx
+++ src/components/MultiplesImagenes.jsx
@@ -7,32 +7,7 @@ export const MultiplesImagenes = (props) => {
   const [image, setImage] = useState({ array: {} });
   const [loading, setLoading] = useState("");
   const handleDrop = (files) => {
-    const uploaders = files.map((file) => {
-      const formData = new FormData();
-      formData.append("file", file);
-      formData.append("tags", `codeinfuse, medium, gist`);
-      formData.append("upload_preset", "Learning");
-      formData.append("api_key", "155765619734214");
-      formData.append("timestamp", (Date.now() / 1000) | 0);
-      setLoading("true");
-      return axios
-        .post(
-          "https://api.cloudinary.com/v1_1/dt9rc5b91/image/upload",
-          formData,
-          {
-            headers: { "X-Requested-With": "XMLHttpRequest" },
-          }
-        )
-        .then((response) => {
-          const data = response.data;
-          console.log(data);
-          const fileURL = data.secure_url;
-          console.log(fileURL);
-        });
-    });
-    axios.all(uploaders).then(() => {
-      setLoading("false");
-    });
+    console.log(files);
   };
   return (
     <div>
