bcfc97c4094285e3f6421d84761bf67485b3b3d1
leonelborgesdev <leonel03t@gmail.com>
2023-08-03 23:23:27 -0400

    Navegacion css

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,5 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./Nav.css";
 
 export const Nav = () => {
-  return <div>Nav</div>;
+  return (
+    <div className="container_nav">
+      <Link to={"/products"}>Productos</Link>
+      <Link to={"/sales"}>Ventas</Link>
+    </div>
+  );
 };
