bb2a91ba076c7f78e292b925abfccc6732426b07
leonelborgesdev <leonel03t@gmail.com>
2023-08-03 23:17:28 -0400

    Home

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import "./App.css";
 import { MultiplesImagenes } from "./components/Multiplesimagenes/MultiplesImagenes";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Sale } from "./components/Sale/Sale";
+import { Home } from "./components/Home/Home";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<MultiplesImagenes />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/product" element={<MultiplesImagenes />} />
           <Route path="/sale" element={<Sale />} />
         </Routes>
       </BrowserRouter>
