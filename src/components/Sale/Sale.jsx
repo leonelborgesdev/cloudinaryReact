56d164e844d05f6a6c8f87add611c45a1df52f1d
leonelborgesdev <leonel03t@gmail.com>
2023-08-03 23:40:17 -0400

    sales css

--- src/components/Sale/Sale.jsx
+++ src/components/Sale/Sale.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import "./Sales.css";
 
 export const Sale = () => {
   const navigate = useNavigate();
   return (
     <div>
       <h2>Ventas</h2>
-      <button onClick={() => navigate("/")}>Volver</button>
+      <button className="button_back" onClick={() => navigate("/")}>
+        Volver
+      </button>
     </div>
   );
 };
