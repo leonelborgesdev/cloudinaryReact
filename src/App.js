57212c5ef329c044845a6180614630e2aa4ebc63
leonelborgesdev <leonel03t@gmail.com>
2023-08-03 23:15:04 -0400

    sales

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 import { MultiplesImagenes } from "./components/Multiplesimagenes/MultiplesImagenes";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Sale } from "./components/Sale/Sale";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<MultiplesImagenes />} />
+          <Route path="/sale" element={<Sale />} />
         </Routes>
       </BrowserRouter>
     </div>
